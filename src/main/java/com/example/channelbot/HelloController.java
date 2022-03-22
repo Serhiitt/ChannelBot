9003ad01f696d329bc5b3873e0e3cf744edd3a2f
Serhiitt <sergey.telezhnikov2517@gmail.com>
2022-03-22 21:54:00 +0200

    add make screen

--- src/main/java/com/example/channelbot/HelloController.java
+++ src/main/java/com/example/channelbot/HelloController.java
@@ -3,6 +3,12 @@ package com.example.channelbot;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
@@ -13,5 +19,30 @@ public class HelloController {
     }
 
     @FXML
+    private void start() {
+        welcomeText.setText("Программа выполниться через ... секунд");
+
+    }
+
+    @FXML
+    private void screen(){
+        try {
+            Thread.sleep(120);
+            Robot r = new Robot();
+
+            // It saves screenshot to desired path
+            String path = "Shot_2.jpg";
+
+            // Used to get ScreenSize and capture image
+            Rectangle capture = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage Image = r.createScreenCapture(capture);
+            ImageIO.write(Image, "jpg", new File(path));
+            System.out.println("Screenshot saved");
+        }
+        catch (AWTException | IOException | InterruptedException ex) {
+            System.out.println(ex);
+        }
+    }
+
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.channelbot {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.channelbot to javafx.fxml;
